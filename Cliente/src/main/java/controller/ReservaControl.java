ff30ba8044168519b2b50edc103448b658c4c609
caligu17 <mcatalinalizarazog17@gmail.com>
2024-09-24 15:02:17 -0500

    cambio normbre controlador

--- Cliente/src/main/java/controller/ReservaControl.java
+++ Cliente/src/main/java/controller/ReservaControl.java
@@ -3,7 +3,7 @@ package controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class ReservaControl {
     @FXML
     private Label welcomeText;
 
