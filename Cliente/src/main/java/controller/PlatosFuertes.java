1b12c27677579ea3e8c60c32bf793686745c755a
NicoPedraza2001 <nicopedraza7@gmail.com>
2024-10-02 11:46:47 -0500

    Conexion entre pantallas

--- Cliente/src/main/java/controller/PlatosFuertes.java
+++ Cliente/src/main/java/controller/PlatosFuertes.java
@@ -2,9 +2,13 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class PlatosFuertes {
 
@@ -20,6 +24,8 @@ public class PlatosFuertes {
     public Button selecAtras;
     @FXML
     public Button selecMenu;
+    @FXML
+    public Button selecVolver;
     @FXML
     public Button selecHome;
     @FXML
@@ -52,15 +58,98 @@ public class PlatosFuertes {
     public Button risotoHongos;
 
     public void onClickPizza(ActionEvent actionEvent) {
+        try {
+            // Carga la nueva ventana
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/vista/PizzaMenu.fxml"));
+            Parent root = loader.load();
+
+            // Crea una nueva escena
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.setTitle("Menu Pizza"); // Título de la nueva ventana
+            stage.show();
+
+            //Opcionalmente, cierra la ventana actual
+            ((Stage) selecPizza.getScene().getWindow()).close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void onClickPasta(ActionEvent actionEvent) {
+        try {
+            // Carga la nueva ventana
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/vista/PastaMenu.fxml"));
+            Parent root = loader.load();
+
+            // Crea una nueva escena
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.setTitle("Menu Pasta"); // Título de la nueva ventana
+            stage.show();
+
+            //Opcionalmente, cierra la ventana actual
+            ((Stage) selecPasta.getScene().getWindow()).close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void onClickLa(ActionEvent actionEvent) {
+        try {
+            // Carga la nueva ventana
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/vista/LasanaMenu.fxml"));
+            Parent root = loader.load();
+
+            // Crea una nueva escena
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.setTitle("Menu Lasaña"); // Título de la nueva ventana
+            stage.show();
+
+            //Opcionalmente, cierra la ventana actual
+            ((Stage) selecLasagna.getScene().getWindow()).close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void onClickRisoto(ActionEvent actionEvent) {
+        try {
+            // Carga la nueva ventana
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/vista/RisotoMenu.fxml"));
+            Parent root = loader.load();
+
+            // Crea una nueva escena
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.setTitle("Menu Risoto"); // Título de la nueva ventana
+            stage.show();
+
+            //Opcionalmente, cierra la ventana actual
+            ((Stage) selecRisoto.getScene().getWindow()).close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onClickVolver(ActionEvent actionEvent) {
+        try {
+            // Carga la nueva ventana
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/vista/PlatosFuertes.fxml"));
+            Parent root = loader.load();
+
+            // Crea una nueva escena
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.setTitle("Platos Fuertes"); // Título de la nueva ventana
+            stage.show();
+
+            //Opcionalmente, cierra la ventana actual
+            ((Stage) selecVolver.getScene().getWindow()).close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void onClick(ActionEvent actionEvent) {
@@ -113,6 +202,7 @@ public class PlatosFuertes {
 
     public void onClickHongos(ActionEvent actionEvent) {
     }
+
 }
 
    
