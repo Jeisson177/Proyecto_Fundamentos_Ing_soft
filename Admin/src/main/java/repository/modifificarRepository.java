537f6d9c6a660c64279a8e6a7c4f0f3810e61392
Jeisson177 <79467624+Jeisson177@users.noreply.github.com>
2024-10-23 18:27:30 -0500

    finalizado modificar mesas

--- Admin/src/main/java/repository/modifificarRepository.java
+++ Admin/src/main/java/repository/modifificarRepository.java
@@ -1,4 +1,69 @@
 package repository;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+import javafx.geometry.Point2D;
+
 public class modifificarRepository {
+    private String url = "jdbc:mysql://localhost:3306/proyecto ingesoft";
+    private String user = "root";
+    private String password = "cl";
+
+    public Map<Integer, Point2D> obtenerPosicionesMesas() throws SQLException {
+        Map<Integer, Point2D> posicionesMesas = new HashMap<>();
+        String query = "SELECT ID_MESA, UBICACION_X, UBICACION_Y FROM MESA";
+
+        try (Connection connection = DriverManager.getConnection(url, user, password);
+             PreparedStatement statement = connection.prepareStatement(query);
+             ResultSet resultSet = statement.executeQuery()) {
+
+            while (resultSet.next()) {
+                int idMesa = resultSet.getInt("ID_MESA");
+                double ubicacionX = resultSet.getDouble("UBICACION_X");
+                double ubicacionY = resultSet.getDouble("UBICACION_Y");
+                posicionesMesas.put(idMesa, new Point2D(ubicacionX, ubicacionY));
+            }
+        }
+        return posicionesMesas;
+    }
+
+    public Point2D obtenerPosicionMesa(int idMesa) throws SQLException {
+        String query = "SELECT UBICACION_X, UBICACION_Y FROM MESA WHERE ID_MESA = ?";
+
+        try (Connection connection = DriverManager.getConnection(url, user, password);
+             PreparedStatement statement = connection.prepareStatement(query)) {
+
+            statement.setInt(1, idMesa);
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                double ubicacionX = resultSet.getDouble("UBICACION_X");
+                double ubicacionY = resultSet.getDouble("UBICACION_Y");
+                return new Point2D(ubicacionX, ubicacionY);
+            }
+        }
+        return null;
+    }
+
+    // Actualizar posición de una mesa en la base de datos
+    public void actualizarPosicionMesa(int idMesa, double nuevaX, double nuevaY) throws SQLException {
+        String query = "UPDATE MESA SET UBICACION_X = ?, UBICACION_Y = ? WHERE ID_MESA = ?";
+
+        try (Connection connection = DriverManager.getConnection(url, user, password);
+             PreparedStatement statement = connection.prepareStatement(query)) {
+
+            statement.setDouble(1, nuevaX);
+            statement.setDouble(2, nuevaY);
+            statement.setInt(3, idMesa);
+
+            statement.executeUpdate();
+        }
+    }
+
+
 }
