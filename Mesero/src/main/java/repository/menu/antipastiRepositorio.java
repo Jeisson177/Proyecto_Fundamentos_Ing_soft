de64b3ec23b617d6dc5dc2b787348c78b8ed2ac0
caligu17 <mcatalinalizarazog17@gmail.com>
2024-10-24 09:20:23 -0500

    Primeras conexiones con BD para disponibilidad
    creación repositorio antipasti

--- Mesero/src/main/java/repository/menu/antipastiRepositorio.java
+++ Mesero/src/main/java/repository/menu/antipastiRepositorio.java
@@ -6,16 +6,17 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class bebidasRepositorio {
+public class antipastiRepositorio {
+    //String url = "jdbc:mysql://localhost:3307/proyecto ingesoft";
     private static final String URL = "jdbc:mysql://localhost:3306/proyecto ingesoft";
     private static final String USER = "root";
     private static final String PASSWORD = "cl";
 
-    public int getPrecios(String nombreBebida) {
+    public int getPrecios(String nombre) {
         String query = "SELECT PRECIO FROM PLATO WHERE nombre = ?";
         try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
             try (PreparedStatement stmt = connection.prepareStatement(query)) {
-                stmt.setString(1, nombreBebida);  // Asigna el parámetro
+                stmt.setString(1, nombre);  // Asigna el parámetro
                 try (ResultSet rs = stmt.executeQuery()) {
                     if (rs.next()) {
                         return rs.getInt("PRECIO");
@@ -27,11 +28,11 @@ public class bebidasRepositorio {
         }
         return -1;  // Valor por defecto si no se encuentra el precio
     }
-    public String getDispo(String nombreBebida){
+    public String getDispo(String nombre){
         String query = "SELECT   p.NOMBRE AS Plato, CASE  WHEN MIN(ii.CANTIDAD_INV >= ip.Cantidad) THEN 'Disponible' ELSE 'NO disponible' END AS Dispo FROM  PLATO p JOIN INGREDIENTE_PLATO ip ON p.ID_PLATO = ip.ID_PLATO JOIN INGREDIENTE_INV ii ON ip.ID_INGREDIENTE = ii.ID_INGREDIENTE WHERE p.NOMBRE = ? GROUP BY p.NOMBRE;";
         try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
             try (PreparedStatement stmt = connection.prepareStatement(query)) {
-                stmt.setString(1, nombreBebida);  // Asigna el parámetro
+                stmt.setString(1, nombre);  // Asigna el parámetro
                 try (ResultSet rs = stmt.executeQuery()) {
                     if (rs.next()) {
                         return rs.getString("Dispo");
@@ -44,4 +45,3 @@ public class bebidasRepositorio {
         return "No encontrado";
     }
 }
-
