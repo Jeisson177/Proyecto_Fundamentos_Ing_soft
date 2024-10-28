81386be0553d6252ac2d809d2a8dfa4b3dc6799b
JocelyneGonzalezHernandez <jocelynegh1@gmail.com>
2024-10-28 14:21:19 -0600

    prueba usuarios
    pruebas para crear, autenticar y consultar reservas

--- Cliente/src/test/java/pkg/AppTest.java
+++ Cliente/src/test/java/pkg/AppTest.java
@@ -8,9 +8,9 @@ import repository.UsuarioRepository;
 import services.GestionarReserva;
 
 import java.time.LocalTime;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class AppTest {
@@ -26,11 +26,84 @@ public class AppTest {
     }
 
     @Test
-    void obtenerHorarioDia_DiaExistente() {
-        ReservaRepository.HorarioDia horario = reservaRepo.obtenerHorarioDia("Lunes");
-        assertNotNull(horario, "El horario para el día especificado debería existir");
-        assertEquals(30, horario.getIntervalo());
-        assertEquals(LocalTime.of(9, 0), horario.getHoraApertura());
-        assertEquals(LocalTime.of(18, 0), horario.getHoraCierre());
+    void autentificarUsuario_UsuarioExistente() {
+        // Configuración de los datos de prueba
+        String email = "juanperez@email.com";
+        String password = "contrasena1";
+
+        // Ejecución de la prueba
+        int userId = usuarioRepo.AutentificarUsuario(email, password);
+
+        // Verificación del resultado
+        assertTrue(userId > 0, "El ID de usuario debería ser mayor a 0 si el usuario existe");
+    }
+
+    @Test
+    void autentificarUsuario_UsuarioInexistente() {
+        // Datos de prueba que no existen en la base de datos
+        String email = "inexistente@example.com";
+        String password = "contrasenaIncorrecta";
+
+        // Ejecución de la prueba
+        int userId = usuarioRepo.AutentificarUsuario(email, password);
+
+        // Verificación de que el usuario no fue encontrado
+        assertEquals(-1, userId, "El ID de usuario debería ser -1 si el usuario no existe o las credenciales son incorrectas");
+    }
+
+    @Test
+    void crearUsuario_Exito() {
+        // Configuración de los datos para crear un usuario
+        String email = "lorenaQ@email.com";
+        String password = "contrasena22";
+        String nombre = "Lorena Quijada";
+        String telefono = "123456789";
+
+        // Ejecución de la prueba
+        int nuevoId = usuarioRepo.CrearUsuario(email, password, nombre, telefono);
+
+        // Verificación del resultado
+        assertTrue(nuevoId > 0, "El ID del nuevo usuario debería ser mayor a 0 si la creación fue exitosa");
+    }
+
+    @Test
+    void crearUsuario_SinExito() {
+        // Configuración de los datos para crear un usuario
+        String email = "choi@email.com";
+        String password = "contrasena23";
+        String nombre = " Seung Choi";
+        String telefono = "";
+
+        // Ejecución de la prueba
+        int nuevoId = usuarioRepo.CrearUsuario(email, password, nombre, telefono);
+
+        // Verificación del resultado
+        assertTrue(nuevoId < 0, "El ID del nuevo usuario debería ser menor a 0 si la creación fue no exitosa");
+    }
+
+    @Test
+    void consultarReservas_UsuarioConReservas() {
+        // Suponiendo que el ID 1 pertenece a un usuario con reservas en la base de datos
+        int idUsuario = 1;  //Juan Perez
+
+        // Ejecución de la prueba
+        List<String> reservas = usuarioRepo.ConsultarReservas(idUsuario);
+
+        // Verificación del resultado
+        assertNotNull(reservas, "La lista de reservas no debería ser nula");
+        assertFalse(reservas.isEmpty(), "La lista de reservas debería contener al menos una reserva para este usuario");
+    }
+
+    @Test
+    void consultarReservas_UsuarioSinReservas() {
+        //ID 22 pertenece a un usuario sin reservas en la base de datos
+        int idUsuario = 22;  //Lorena Q
+
+        // Ejecución de la prueba
+        List<String> reservas = usuarioRepo.ConsultarReservas(idUsuario);
+
+        // Verificación del resultado
+        assertNotNull(reservas, "La lista de reservas no debería ser nula");
+        assertTrue(reservas.isEmpty(), "La lista de reservas debería estar vacía para este usuario sin reservas");
     }
 }
