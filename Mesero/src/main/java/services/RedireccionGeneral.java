a80be63d551691026cf8c3bcb480f93b795fab68
Vicky <koalaxleaf@gmail.com>
2024-10-27 20:54:46 -0500

    Ya se puede seleccionar una reserva, el menu ya esta hecho y aparecen las warnings necesarias. Falta seleccionar la mesa y el proceso de seleccionar los platos del menú para hacer la orden

--- Mesero/src/main/java/services/RedireccionGeneral.java
+++ Mesero/src/main/java/services/RedireccionGeneral.java
@@ -8,7 +8,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class RedireccionGeneral {
+public class  RedireccionGeneral {
 
     public void IrHome(Button Home) {
         try {
