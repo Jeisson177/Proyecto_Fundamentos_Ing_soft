85d3e4ac67a5e1a7f18c59fb25a49a84e69545c1
JocelyneGonzalezHernandez <jocelynegh1@gmail.com>
2024-10-28 13:49:07 -0600

    prueba 1
    prueba obtenerHorarioDia_DiaExistente

--- Cliente/src/test/java/pkg/AppTest.java
+++ Cliente/src/test/java/pkg/AppTest.java
@@ -1,11 +1,36 @@
 package pkg;
 
 import com.example.Cliente.usuarioC;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import repository.ReservaRepository;
+import repository.UsuarioRepository;
+import services.GestionarReserva;
+
+import java.time.LocalTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
 public class AppTest {
+    private ReservaRepository reservaRepo;
+    private UsuarioRepository usuarioRepo;
+    private GestionarReserva gestionarReserva;
+
+    @BeforeEach
+    void setUp() {
+        reservaRepo = new ReservaRepository();
+        usuarioRepo = new UsuarioRepository();
+        //gestionarReserva = new GestionarReserva(new com.example.Cliente.MesaControl());
+    }
+
     @Test
-    void probarHolaMundo() {
+    void obtenerHorarioDia_DiaExistente() {
+        ReservaRepository.HorarioDia horario = reservaRepo.obtenerHorarioDia("Lunes");
+        assertNotNull(horario, "El horario para el día especificado debería existir");
+        assertEquals(30, horario.getIntervalo());
+        assertEquals(LocalTime.of(9, 0), horario.getHoraApertura());
+        assertEquals(LocalTime.of(18, 0), horario.getHoraCierre());
     }
 }
