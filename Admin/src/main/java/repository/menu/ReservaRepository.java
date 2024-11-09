f02ee6d51cf367423410d991e29a5637fe0573d4
Jeisson177 <79467624+Jeisson177@users.noreply.github.com>
2024-11-09 18:41:53 -0500

    get credenciales

--- Admin/src/main/java/repository/menu/ReservaRepository.java
+++ Admin/src/main/java/repository/menu/ReservaRepository.java
@@ -1,6 +1,7 @@
 package repository.menu;
 
 import javafx.scene.control.Alert;
+import repository.Credenciales;
 
 import java.sql.*;
 import java.time.LocalDate;
@@ -10,9 +11,11 @@ import java.time.format.DateTimeFormatter;
 
 public class ReservaRepository {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/proyecto ingesoft";
-    private static final String USER = "root";
-    private static final String PASSWORD = "cl";
+    private static final Credenciales c=new Credenciales();
+
+    private static final String URL = c.getURL();
+    private static final String USER = c.getUser();
+    private static final String PASSWORD = c.getPassword();
 
     // Clase interna para encapsular la información del horario del día
     public static class HorarioDia {
