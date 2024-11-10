0a1c9de7e0574a9528b1fcfe6c66bb7f9f25c15c
NicoPedraza2001 <nicopedraza7@gmail.com>
2024-11-10 13:18:36 -0500

    Botones de direccionamiento

--- Admin/src/main/java/controller/modificarInventario.java
+++ Admin/src/main/java/controller/modificarInventario.java
@@ -1,18 +1,30 @@
 package controller;
 
+import controller.menu.RedireccionGeneral;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import repository.modificarInventarioRepository;
 
 
 import java.util.Date;
+import java.util.Objects;
 
 public class modificarInventario {
 
+    @FXML
+    public Button selecHome;
+    @FXML
+    public ImageView instaImagen;
+    @FXML
+    public Button selecMenu;
     @FXML
     private TableView<modificarInventarioRepository.InventarioAlimento> tablaInventario;
     @FXML
@@ -24,16 +36,23 @@ public class modificarInventario {
     @FXML
     private TableColumn<modificarInventarioRepository.InventarioAlimento, String> columnaUnidad;
 
+    private RedireccionGeneral Ira=new RedireccionGeneral();
+
     private modificarInventarioRepository repository = new modificarInventarioRepository();
 
 
     @FXML
     public void initialize() {
+
+        Image ig = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/imagenes/PlatosFuertes/ig.png")));
+
         // Configurar las columnas para asociarse con los campos de InventarioAlimento
         columnaNombre.setCellValueFactory(new PropertyValueFactory<>("nombre"));
         columnaCantidad.setCellValueFactory(new PropertyValueFactory<>("cantidadInv"));
         columnaFecha.setCellValueFactory(new PropertyValueFactory<>("fechaVen"));
         columnaUnidad.setCellValueFactory(new PropertyValueFactory<>("unidad"));
+
+        instaImagen.setImage(ig);
 
         // Cargar los datos en la tabla
         cargarDatosTabla();
@@ -47,4 +66,9 @@ public class modificarInventario {
     }
 
 
+    public void IrHome(ActionEvent actionEvent) { Ira.IrHome(selecHome);
+    }
+
+    public void IrMenu(ActionEvent actionEvent) { Ira.IrMenu(selecMenu);
+    }
 }
