391cad609cb4199bbbec4a7c1972ccac897e4be6
caligu17 <mcatalinalizarazog17@gmail.com>
2024-11-11 17:35:30 -0500

    segundo intento h2

--- Mesero/src/main/java/persistencia/DBConnectionManager.java
+++ Mesero/src/main/java/persistencia/DBConnectionManager.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DBConnectionManager implements IDBConnectionManager {
 
-    private static final String JDBC_URL = "jdbc:h2:file:./out/Cliente";
+    private static final String JDBC_URL = "jdbc:h2:file:C:/Users/lenovo1/Documents/GitHub/Proyecto_Fundamentos_Ing_soft/db/Cliente";
 
     private static final String USER = "sa";
     private static final String PASSWORD = "";
