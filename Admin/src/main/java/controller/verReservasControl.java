c030aa0a27ee39dbb34809ab235c6f6f7c5deba3
caligu17 <mcatalinalizarazog17@gmail.com>
2024-11-12 13:46:39 -0500

    adaptación pantalla

--- Admin/src/main/java/controller/verReservasControl.java
+++ Admin/src/main/java/controller/verReservasControl.java
@@ -1,4 +1,15 @@
 package controller;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
 public class verReservasControl {
+    public Button regresarBoton;
+    public Button buscarBoton;
+    public TextField nombreClienteBoton;
+    public TextField idReservaBoton;
+    public DatePicker ingresoFecha;
+    public Text textoReservas;
 }
